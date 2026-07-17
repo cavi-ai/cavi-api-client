b94e1b509e9cb891b6f18b8cebb188e1f9cb31f3
Sasan Sotoodehfar <sasan1200@gmail.com>
2026-07-17 17:43:38 -0400

    feat(testing): team directory conformance kit

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -3,5 +3,6 @@ export * from "./runtime-control-client-conformance.js";
 export * from "./runtime-control-scenarios.js";
 export * from "./raw-gateway-conformance.js";
 export * from "./kanban-conformance.js";
+export * from "./team-directory-conformance.js";
 export * from "./transport-conformance.js";
 export type { TransportKind } from "../core/transport/index.js";
